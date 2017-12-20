e7f5cbb2391f0569088d936500a40e7d8a1d78d3
Ziqiang Lu <lzq8239@163.com>
2017-12-20 09:59:29 +0800

    fix

---
+++ src/main/java/com/greentown/smscenter/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.greentown.smscenter.mapper;
+
+import java.util.List;
+
+import com.greentown.smscenter.model.dto.UserDO;
+public interface UserMapper {
+
+	List<UserDO> getUserList();
+	
+}
